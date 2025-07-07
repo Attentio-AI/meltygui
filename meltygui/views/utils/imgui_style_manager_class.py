18c224912e281932357baece55af675a354af269
Lukas Valine <lukas@valine.io>
2025-07-06 22:32:49 -0500

    Refactor ImGui border color configuration
    Move the definition of imgui.COLOR_BORDER to the window backgrounds section and adjust its value from 0.3 to 0.2 in meltygui/views/utils/imgui_style_manager_class.py. This consolidates background-related style properties and updates the border appearance.

--- meltygui/views/utils/imgui_style_manager_class.py
+++ meltygui/views/utils/imgui_style_manager_class.py
@@ -235,9 +235,10 @@ class ImGuiStyleManager:
         colors[imgui.COLOR_TEXT_DISABLED] = make_color(0.50, 0.2)  # Grayed out text
 
         # Window backgrounds
-        colors[imgui.COLOR_WINDOW_BACKGROUND] = make_color(0.12, 0.5)  # Window background
+        colors[imgui.COLOR_WINDOW_BACKGROUND] = make_color(0.1, 0.5)  # Dark background
         colors[imgui.COLOR_CHILD_BACKGROUND] = make_color(0.10, 0.3, 0.0)
         colors[imgui.COLOR_POPUP_BACKGROUND] = make_color(0.9, 0.3)
+        colors[imgui.COLOR_BORDER] = make_color(0.2, 0.7)
 
         # Headers
         colors[imgui.COLOR_HEADER] = make_color(0.35, 0.8, 0.0)
@@ -271,7 +272,6 @@ class ImGuiStyleManager:
         colors[imgui.COLOR_TITLE_BACKGROUND_COLLAPSED] = make_color(0.12, 0.5)
 
         # Borders and separators
-        colors[imgui.COLOR_BORDER] = make_color(0.3, 0.7)
         colors[imgui.COLOR_SEPARATOR] = make_color(0.40, 0.7)
 
         # Sliders, scrollbars
